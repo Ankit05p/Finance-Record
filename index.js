84661a02dad8a7763ae048bdb64ca67ef3d85f89
Ankit05p <ankitpatel260605@gmail.com>
2026-07-15 00:54:40 +0530

    feat(config): register goal and stock routes, initialize monthly cron job

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const Userroutes = require("./routes/UserRoute");
 const CategoryRoutes = require("./routes/CategoryRoute");
 const FinancilaRoutes = require("./routes/FinancialRoute");
 const RecentRoutes = require("./routes/RecentActivity");
+const goalRoutes = require("./routes/GoalRoute.js");
+const {startGoalCron} = require("./utils/GoalCron.js");
+const stockRoutes = require('./routes/StockRoute');
 
 
 
 const PORT = process.env.PORT || 4000;
 dbconnect.dbconnect();
+// startGoalCron();
 app.use(express.json());
 app.use(cookieParser());
 
@@ -21,6 +25,8 @@ app.use("/api/v1/auth",Userroutes);
 app.use("/api/v1/category",CategoryRoutes);
 app.use("/api/v1/finance",FinancilaRoutes);
 app.use("/api/v1/recent",RecentRoutes);
+app.use("/api/v1/goals", goalRoutes);
+app.use('/api/v1/stocks', stockRoutes);
 
 app.get("/",(req,res) => 
 {
